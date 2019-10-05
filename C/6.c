d0e6462a54b2c58f05b5a9b8b2dae2fa5d2c6b07
Nipun Sharma <sharmanipun2902@gmail.com>
2019-10-05 13:06:37 +0530

    Fifth

---
+++ C/6.c
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdio.h>
+
+void main()
+{
+    clrscr();
+    int a,b,c,d;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    c=a/b;
+    d=a%b;
+    printf("Quotient is: %d \n",c);
+    printf("Remainder is: %d \n",d);
+    getch();
+
+}
