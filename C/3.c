b808748ccd8afb93507c6027e9df09364335aa3c
kunalkingsethi <kunalkingsethi@gmail.com>
2019-10-05 12:26:54 +0530

    first commit

---
+++ C/3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,sum;
+    clrscr();
+    printf("Enter the first number :");
+    scanf("%d",&a);
+    printf("Enter the second number :");
+    scanf("%d".&b);
+    sum=a+b;
+    printf("The sum is = %d",sum);
+    getch();
+}
