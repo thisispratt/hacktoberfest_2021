4ea67aa3ac8dfa014e80425d5b22a0242e27edac
kunalkingsethi <kunalkingsethi@gmail.com>
2019-10-05 13:10:16 +0530

    new4

--- C/6.c
+++ C/6.c
@@ -2,15 +2,16 @@
 #include<conio.h>
 void main()
 {
-    int a,b,q,r;
+    int a,b,q,rem;
     clrscr();
     printf("Enter a number :");
     scanf("%d",&a);
     printf("\n Enter the number you want to divide the first number with :");
     scanf("%d",&b);
-    r=a%b;
+    rem=a%b;
     q=a/b;
-    printf("\n The remainder is : %d",r);
+    printf("\n The remainder is : %d",rem);
     printf("\n The quotient is :%d",q);
     getch();
+    
 }
