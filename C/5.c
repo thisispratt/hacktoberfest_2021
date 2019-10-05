b415bca495511dc372171a7f3976844f8dd94d20
Sneha <snehasah2204@gmail.com>
2019-10-05 13:12:09 +0530

    changes

--- C/5.c
+++ C/5.c
@@ -1,3 +1,10 @@
 //finding ascii values of a charecter
 #include<stdio.h>
-#include<
+#include<conio.h>
+main()
+{
+    char c;
+    printf("Enter the charecter \n");
+    scanf("%c",c);
+    printf("The ascii value of %c is %d /n",c,c);
+}
