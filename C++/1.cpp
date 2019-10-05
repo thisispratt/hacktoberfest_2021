ed75c330f3d606ee53b36846e4b3300b4de40268
Nipun Sharma <sharmanipun2902@gmail.com>
2019-10-05 12:22:57 +0530

    First program

---
+++ C++/1.cpp
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<iostream.h>
+
+void main()
+{clrscr();
+ cout<<"                                  ***********\n";
+ cout<<"                                  Hello World\n";
+ cout<<"                                  ***********\n\n";
+ cout<<"                     This is my first github pull program.\n\n";
+ getch();
+}
