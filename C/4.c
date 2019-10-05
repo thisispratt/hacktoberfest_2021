bc34941b808bd932cfd474071dda6ea9415d9a0e
Nipun Sharma <sharmanipun2902@gmail.com>
2019-10-05 12:54:22 +0530

    Fourth

--- C/4.c
+++ C/4.c
@@ -6,9 +6,9 @@ void main()
     clrscr();
     float a,b,x=0;
     printf("Enter first number: ");
-    scanf("%d",&a);
+    scanf("%f",&a);
     printf("Enter second number: ");
-    scanf("%d",&b);
+    scanf("%f",&b);
     x=a*b;
     printf("The product of the numbers is: %d",x);
     getch();
