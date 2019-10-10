040c49e817d62947b39d68903e51f048a61f9f17
NishanthMolleti <48158364+NishanthMolleti@users.noreply.github.com>
2019-10-10 19:46:17 +0530

    first commit

---
+++ C++/6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int a ,b,c,d;
+    cout<<"Enter a value:";
+    cin>>a;
+    cout<<"Enter b value:";
+    cin>>b;
+    c=a/b;
+    cout<<"Remainder is ="<<c;
+    d=a%b;
+    cout<<"Quotient is ="<<d;
+}
