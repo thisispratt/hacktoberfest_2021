155e0a35cd1c0701caab6d190efc4120dbdc868c
NishanthMolleti <48158364+NishanthMolleti@users.noreply.github.com>
2019-10-10 19:35:55 +0530

    updated 1

--- C/1.c
+++ C/1.c
@@ -1,11 +1,5 @@
-#include<conio.h>
 #include<stdio.h>
-
-void main()
-{clrscr();
- printf("                                  ***********\n");
- printf("                                  Hello World\n");
- printf("                                  ***********\n\n");
- printf("                     This is my first github pull program.\n\n");
- getch();
+main()
+{
+    printf("Hello World");
 }
