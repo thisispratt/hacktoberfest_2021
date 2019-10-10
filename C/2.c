28071a6177786a9b489c75d362d35daba6dbdeca
NishanthMolleti <48158364+NishanthMolleti@users.noreply.github.com>
2019-10-10 16:59:29 +0530

    Updated 2nd problem

--- C/2.c
+++ C/2.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
-#include<conio.h>
-
-void main()
+main()
 {
-    clrscr();
     int x;
     printf("Enter a number: ");
     scanf("%d",&x);
     printf("The entered number is: %d",x);
-    getch();
 }
