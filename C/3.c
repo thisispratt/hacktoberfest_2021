22c02767661d026d97be29776e6252681b5ea020
NishanthMolleti <48158364+NishanthMolleti@users.noreply.github.com>
2019-10-10 19:39:12 +0530

    updated 3

--- C/3.c
+++ C/3.c
@@ -1,16 +1,12 @@
 #include<stdio.h>
-#include<conio.h>
-
-void main()
+main()
 {
     int a,b,sum;
-    clrscr();
     printf("Enter the first number :");
     scanf("%d",&a);
     printf("Enter the second number :");
     scanf("%d",&b);
     sum=a+b;
     printf("The sum is = %d",sum);
-    getch();
 }
 
