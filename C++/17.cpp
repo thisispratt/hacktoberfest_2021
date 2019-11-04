45fc28d134d332a138df56186c226afefa40b812
NishanthMolleti <48158364+NishanthMolleti@users.noreply.github.com>
2019-11-04 14:35:31 +0530

    Update 17.cpp

---
+++ C++/17.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+  int n , sum=0;
+  cout<<"Enter a number:";
+  cin>>n;
+  for(i=1;i<=n;i++)
+  {
+    sum=sum+i;
+  }
+  cout<<"Sum is: "<<sum;
+}
