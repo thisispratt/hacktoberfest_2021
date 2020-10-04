7b47df07d884c2da63825eb73c1590320e57a5d1
ssj1901 <stevejacob1901@gmail.com>
2020-10-04 18:49:10 +0530

    Made changes for 28.c

--- C/28.c
+++ C/28.c
@@ -3,21 +3,22 @@ int main() {
     int n, i, k=0;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
+    if (n==1) {
+        printf("1 is neither prime nor composite.");
+    }
+    else {
  for (i=2;i<=n/2;i++) {
         if (n%i==0) {
             k=1;
             break;
         }
     }
-    if (n==1) {
-        printf("1 is neither prime nor composite.");
-    }
-    else {
-        if (k==0)
+  }
+      if (k==0)
             printf("%d is a prime number.", n);
-        else
+      else
             printf("%d is not a prime number.", n);
-    }
+
 
     return 0;
 }
