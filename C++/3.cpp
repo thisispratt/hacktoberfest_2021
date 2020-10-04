cef33cbad3936a872cdb059ccb51bdacf7712da5
Prashant <54770118+9797prashant@users.noreply.github.com>
2020-10-04 15:48:22 +0000

    Updated 3.cpp

--- C++/3.cpp
+++ C++/3.cpp
@@ -1,9 +1,14 @@
+/* C Program to Add Two Integers */
 #include<iostream>
 
-void main()
+int main()
 {
+    //variable declaration
     int a,b;
+    //taking input from users
     cout<<"Enter 2 numbers"<<endl;
     cin>>a>>b;
     cout<<a+b;
+    
+    return 0;
 }
