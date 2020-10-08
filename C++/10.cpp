abf4589afc4f5a2d3923d0d6c0e7d55c647fd658
harish3600 <harishumasankar@students.iiit.ac.in>
2020-10-08 11:37:56 +0400

    Updated 10.cpp and removed bits/stdc++

--- C++/10.cpp
+++ C++/10.cpp
@@ -1,12 +1,13 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int main()
-{long long int a;
-  cout<<"Enter a number"<<endl;
-  cin>>a;
-  if(a%2)
-  	cout<<a<<" is odd."<<endl;
+{
+  long long int a;
+  cout << "Enter a number" << endl;
+  cin >> a;
+  if (a % 2)
+    cout << a << " is odd." << endl;
   else
-  	cout<<a<<" is even."<<endl;
+    cout << a << " is even." << endl;
   return 0;
 }
