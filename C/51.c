f86fedeb4f523915fe7067721ae393ba10374660
Amlana-P <50160346+Amlana-P@users.noreply.github.com>
2020-10-08 15:23:29 +0530

    Add files via upload

---
+++ C/51.c
@@ -0,0 +1,28 @@
+# Soltion to problem 51 in C
+
+#include <stdio.h>
+
+void cyclicSwap(int *a, int *b, int *c);
+
+int main() {
+    int a, b, c;
+
+    printf("Enter a, b and c respectively: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    cyclicSwap(&a, &b, &c);
+
+    printf("Value after swapping:\n");
+    printf("a = %d \nb = %d \nc = %d", a, b, c);
+
+    return 0;
+}
+
+void cyclicSwap(int *n1, int *n2, int *n3) {
+    int temp;
+    // swapping in cyclic order
+    temp = *n2;
+    *n2 = *n1;
+    *n1 = *n3;
+    *n3 = temp;
+}
