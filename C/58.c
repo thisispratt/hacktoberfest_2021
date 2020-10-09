a6ebada97ff85f00b361de6741bf15938d1ed02e
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2020-10-09 15:32:19 +0530

    Update 58.c

---
+++ C/58.c
@@ -0,0 +1,13 @@
+//Program to Copy String Without Using strcpy()
+#include <stdio.h> 
+int main() 
+{ 
+	char s1[] = "Hello World", s2[100], i; 
+	printf("string s1 : %s\n", s1); 
+	for (i = 0; s1[i] != '\0'; ++i) { 
+		s2[i] = s1[i]; 
+	} 
+	s2[i] = '\0'; 
+	printf("String s2 : %s", s2); 
+	return 0; 
+} 
