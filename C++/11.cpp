3581da7ea150ce6277182a2bc2f2ba42af1f8045
Divetri Ayu Rahmawati <54519415+divetri@users.noreply.github.com>
2020-10-10 21:49:50 +0700

    Update 11.cpp

---
+++ C++/11.cpp
@@ -0,0 +1,15 @@
+#include <iostream.h>
+using namespace std;
+int main ()
+{
+	char i;
+  cout<<"Input one alphabet= ";
+  cin>>i;
+  if(i=='a'||i=='i'||i=='u'||i=='e'||i=='o'||i=='A'||i=='I'||i=='U'||i=='E'||i=='O'){
+    cout << "Character is vowel";
+  }
+  else{
+	  cout << "Character is consonant";
+  }
+    return 0;
+}
