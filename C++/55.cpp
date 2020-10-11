6014535b5455793943733514ba9d172eabd3eaba
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2020-10-11 16:56:25 +0530

    Update and rename 51.cpp to 55.cpp

---
+++ C++/55.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string line;
+    string temp = "";
+
+    cout << "Enter a string: ";
+    getline(cin, line);
+
+    for (int i = 0; i < line.size(); ++i) {
+        if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z')) {
+            temp = temp + line[i];
+        }
+    }
+    line = temp;
+    cout << "Output String: " << line;
+    return 0;
+}
+//Output
+
+//Enter a string: p2'r"o@gram84iz./
+//Output String: programiz
