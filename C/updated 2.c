ad88257b16a7b0323f3d71e3b2b8df80e5a4dba6
Rupali409 <54534133+Rupali409@users.noreply.github.com>
2020-10-12 16:29:29 +0530

    created updated2.c
    please check

---
+++ C/updated 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    clrscr();
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    printf("\n The entered number is : %d",num);
+    return(0);
+}
