607323e38ded6ba8835cec6878ada8877cc70aa9
Rupali409 <54534133+Rupali409@users.noreply.github.com>
2020-10-12 16:36:58 +0530

    Created 9.c
    I had swap numbers without use of third variable.
    please check.

---
+++ C/created 9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>  
+ int main()    
+{    
+int a,b;
+printf("Enter num1:");
+scanf("%d",&a);
+printf("Enter num2:");
+scanf("%d",&b);
+printf("Before swap a=%d b=%d",a,b);      
+a=a+b;//a=30 (10+20)    
+b=a-b;//b=10 (30-20)    
+a=a-b;//a=20 (30-10)    
+printf("\nAfter swap a=%d b=%d",a,b);    
+return (0);  
+}   
