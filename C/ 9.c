d66195a97690c866f753d1840cc7c730f6c21978
Rupali409 <54534133+Rupali409@users.noreply.github.com>
2020-10-13 22:19:08 +0530

    9.c
    please check now. created 9.c. swap of 2 numbers without use of 3rd variable.

--- C/ 9.c
+++ C/ 9.c
@@ -7,9 +7,9 @@ scanf("%d",&a);
 printf("Enter num2:");
 scanf("%d",&b);
 printf("Before swap a=%d b=%d",a,b);      
-a=a+b;//a=30 (10+20)    
-b=a-b;//b=10 (30-20)    
-a=a-b;//a=20 (30-10)    
+a=a+b;    
+b=a-b;    
+a=a-b; 
 printf("\nAfter swap a=%d b=%d",a,b);    
 return (0);  
 }   
