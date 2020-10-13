aee4c0f63210353bc7af5122b9b2f00e338046a5
smv1999 <vaidhyanathan.sm@gmail.com>
2020-10-13 11:31:14 +0530

    added cpp program number 58

---
+++ C++/58.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string str, res = "";
+    cin >> str;
+    int index = 0, len = str.length();
+    for (index = 0; index < len; index++)
+    {
+        res += str[index];
+    }
+    cout << res;
+    return 0;
+}
