f9e470933c1e501914de82dde2d62293decf9c84
Ayush Saini <62163030+AyushSaini00@users.noreply.github.com>
2020-10-13 19:50:54 +0530

    44 by dynamic array

--- C++/44.cpp
+++ C++/44.cpp
@@ -3,19 +3,26 @@ using namespace std;
 
 // program to find maximum number in array of max size 10
 int main(){
-    int arr[10];
-    int n;
-    cout<<"Enter the size of array(max:10) : ";
-    cin>>n;
-    cout<<"Enter the numbers: "<<endl;
-    for(int i = 0; i < n; i++){
-        cin>>arr[i];
+    
+    int *array, size;
+    cout<<"Enter the size of array : ";
+    cin>>size;
+    array = new int[size]; //creating array dynamically with new keyword
+    
+    // taking in the value of array
+    cout<<"Enter the numbers : "<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin>>array[i];
     }
-    for (int i = 1; i < n; i++)
+    //finding the maximum number in array and then placing it at the zeorth index
+    for (int i = 1; i < size; i++)
     {
         if(arr[0] < arr[i])
             arr[0] = arr[i];
     }
     cout<<"Largest number is :"<<arr[0]<<endl;
+    
+    delete[] array;  //releasing memory
     return 0;
 }
