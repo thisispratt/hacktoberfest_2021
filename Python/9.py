e40aa0459cd487df70ac4b267f8cffbc39aae71f
ManavRanawat <manavranawat0@gmail.com>
2020-10-17 02:19:38 +0530

    added 9.py(swapping 2 integers) file

---
+++ Python/9.py
@@ -0,0 +1,13 @@
+#  9.  Program to Swap Two Numbers
+
+#initializing the numbers to be swapped
+a = 10
+b = 20
+print('Before Swapping:-')
+print("a:",a,"b:",b)
+
+#swapping the numbers
+a,b = b,a
+#Now,printing the swapped numbers
+print('After Swapping:-')
+print("a:",a,"b:",b)
