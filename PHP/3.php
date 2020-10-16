32d2de25d86a0fc6f0b61bc23b27d88f2f6a61c9
Muhammad Rafly Indrawan <Fliw@DigitalKode.com>
2020-10-16 07:52:14 +0700

    Adding readable comment | challenge 3

---
+++ PHP/3.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * variable x is valued 10, variable y is valued 5
+ * we can add two varibles directly into echo or even in a variables (Z)
+ */
+$x = 10;
+$y = 5;
+$z = $x + $y;
+echo $z;
+echo $x + $y;
+?>
