0dee84fe0e07f50f061916a1425ac51a833a0625
solarconstant <ashishshubh001@gmail.com>
2020-10-16 10:04:28 +0530

    Add solution to problem 55 in Python

---
+++ Python/55.py
@@ -0,0 +1,14 @@
+# 55. Program to Remove all Characters in a String Except Alphabets
+
+String = "Program to Remove all characters in a String @@@@$^*(@^(*^%# except Al%(*&phab*(@$ets"
+
+alphabets_up = [chr(alpha) for alpha in range(65, 91)]
+alphabets_low = [chr(alpha) for alpha in range(97, 123)]
+
+ans_string = '' #String can't be mutated
+
+for character in String:
+    if character in alphabets_low or character in alphabets_up:
+        ans_string += character
+
+print(ans_string)
