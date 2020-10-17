f1251754106177db530a3f1605d3197be92622ae
Ayush Akash <59696342+ayushakash990@users.noreply.github.com>
2020-10-17 11:28:21 +0530

    56.py

---
+++ Python/56.py
@@ -0,0 +1,5 @@
+# User inputs the string and it gets stored in variable str
+str = input("Enter a string: ")
+
+# using len() function to find length of str
+print("Length of the input string is:", len(str))
