e7afac4b77fcc8b5319172d9bc856a0dbe819520
UsamaEjaz0 <uejaz0@gmail.com>
2020-10-18 02:47:04 +0500

    Added Q2 java

---
+++ Java/Q2.java
@@ -0,0 +1,18 @@
+package palindrome;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+ class Q1 {
+
+    
+
+    public static void main(String[] agrs) {
+        System.out.println("Enter an integer:");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print("You entered: " + a);
+
+    }
+
+}
