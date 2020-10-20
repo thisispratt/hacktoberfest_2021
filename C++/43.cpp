e7adbab74ccdc49a2c2d8a05ce122c2da1d31efd
nevergiv3up <73099613+nevergiv3up@users.noreply.github.com>
2020-10-20 18:05:08 +0530

    Update 43.cpp

--- C++/43.cpp
+++ C++/43.cpp
@@ -1,5 +1,7 @@
 /*Created by Nevergiv3up*/
 
+//This Program is written in C++ language and helps in finding average of n number's in an array.
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -7,23 +9,23 @@ int main()
 {
   long long int n;
   double sum=0;
-  cin>>n;
+  cin>>n;   //Taking input of how many numbers the array will consist of
   if(n<=0)
   {
     cout<<"INVALID INPUT";
   }
   else
   {
-  int array[n];
+  int array[n]; //definining an array of size n
   for(int i=0;i<n;i++)
   {
-    cin>>array[i];
+    cin>>array[i];         //Taking input of array elements
   }
   for (int i = 0; i < n; ++i)
   {
-    sum+=array[i];
+    sum+=array[i]; //Storing the sum of all elements in a variable
   }
-  cout<<"Average of numbers using arrays "<<float(sum/n)<<endl;
+  cout<<"Average of numbers using arrays "<<float(sum/n)<<endl;    //Finding the average
   return 0;
   }
 }
