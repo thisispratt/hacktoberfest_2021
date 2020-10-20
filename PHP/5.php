41ebf04eedd0b90d8ea77e45ba2c3b88e25d5f6b
Muhammad Rafly Indrawan <Fliw@DigitalKode.com>
2020-10-20 22:50:23 +0700

    reverting challenges

--- PHP/5.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Program to Find ASCII Value of a Character
- * 
- * ord() function is a builtin PHP function to get ascii val of char.
- */
-$x = readline("Enter Phrases : ");
-echo "the ascii of phrases : " . ord($x);
-?>
