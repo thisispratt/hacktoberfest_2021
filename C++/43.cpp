4eb686f9d5c2bf8202e7e12d9901f50dd3c5305d
parichay madnani <parichayman2@gmail.com>
2020-10-28 23:21:10 +0530

    added 29 , 43 in cpp

---
+++ C++/43.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    float num[100], sum=0, average;
+
+    cout << "Enter the numbers of data: ";
+    cin >> n;
+
+
+    for(i = 0; i < n; ++i)
+    {
+        cout << i + 1 << ". Enter number: ";
+        cin >> num[i];
+        sum += num[i];
+    }
+
+    average = sum / n;
+    cout << "Average = " << average;
+
+    return 0;
+}
